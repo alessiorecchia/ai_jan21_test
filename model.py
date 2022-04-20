066f1738e66ec1e748c46e7692a259d42a4a8b57
Varun Kota <vkr.k8055@gmail.com>
2022-04-20 10:41:44 +0200

    Update model.py

--- model.py
+++ model.py
@@ -9,4 +9,15 @@ import torch.nn.functional as F
 
 class Classifier(nn.Module):
     def __init__(self):
-        self.input_layer = nn.Conv2d(1,28,)
+        super(Classifier, self).__init__()
+        self.input_layer = nn.Conv2d(1,28,kernel_size=7)
+        self.conv1 = nn.Conv2d(28, 56, 7)
+        self.conv2 = nn.Conv2d(56,112,7)
+        self.drop = nn.Dropout2d(0.1)
+        self.layer1 = nn.Linear(111,60)
+        self.layer2 = nn.Linear(60,10)
+
+    def forward(self, x):
+        x = F.relu(self.drop(self.input_layer(x)))
+        x
+
