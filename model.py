66423c046d6a639a308efba627aaec99ed73d34b
Jeko <57463439+Jeko-Mus@users.noreply.github.com>
2022-04-20 11:33:27 +0200

    Update model.py

--- model.py
+++ model.py
@@ -14,10 +14,23 @@ class Classifier(nn.Module):
         self.conv1 = nn.Conv2d(28, 56, 7)
         self.conv2 = nn.Conv2d(56,112,7)
         self.drop = nn.Dropout2d(0.1)
-        self.layer1 = nn.Linear(111,60)
+        self.layer1 = nn.Linear(112*10*10,60)
         self.layer2 = nn.Linear(60,10)
 
     def forward(self, x):
         x = F.relu(self.drop(self.input_layer(x)))
-        x
+        x = F.relu(self.drop(self.conv1(x)))
+        x = F.relu(self.drop(self.conv2(x)))
+        #print(x.shape)
+        x = x.view(x.shape[0], -1)
+        #print(x.shape)
+        x = F.relu(self.layer1(x))
+        out_layer = self.layer2(x)
+        return out_layer
+
+x = torch.rand((16,1,28,28))
+#print(x, x.shape)
+model = Classifier()
+print(model.forward(x))
+
 
